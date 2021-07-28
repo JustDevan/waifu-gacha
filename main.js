6b9a214bf694304c582a0b4ae65de42ae03a0b8a
JustDevan <justdevan1st@gmail.com>
2021-07-28 16:13:01 +0700

    Menghapus bekas kode ajax

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
 const getImageCard = document.querySelector('.card');
 const getGenerateButton = document.querySelector('.generate');
 
-function ambilGambar(url, success) {
-    let getres = new XMLHttpRequest();
-
-    getres.onreadystatechange = () => {
-        if (getres.readyState === 4) {
-            if (getres.status === 200) {
-                success(getres.response);
-            }
-        }
-    };
-    getres.open('get', url);
-    getres.send();
-}
-
 getGenerateButton.addEventListener('click', function() {
-    // console.log('ditekan!')
-    // getImageCard.innerHTML = '';
-    // ambilGambar('https://api.waifu.pics/sfw/waifu', results => {
-    //     const gambar = JSON.parse(results);
-    //     getImageCard.innerHTML = `<img class="waifu-pic" src=${gambar.url}>`;
-    // });
     getImageCard.innerHTML = ''
     fetch('https://api.waifu.pics/sfw/waifu')
         .then(response => response.json())
