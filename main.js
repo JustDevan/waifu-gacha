266a787a3a4b7159df93165abe6f44e3f9eb3ee4
Devan <Justdevan1st@gmail.com>
2021-08-05 21:33:14 +0700

    Remove Dead Code ( JavaScript )

--- main.js
+++ main.js
@@ -9,31 +9,7 @@ const requestUrl = (url, success) => {
 getContainer.addEventListener('click', el => {
   if(el.target.dataset.type === 'generate'){
     requestUrl('https://api.waifu.pics/sfw/waifu', res => {
-      getMainImage.innerHTML = ''
-      getMainImage.innerHTML += `<a href="${res.url}" target="_blank"><img class="waifu-pic" src="${res.url}"></a>`
+      getMainImage.innerHTML = `<a href="${res.url}" target="_blank"><img class="waifu-pic" src="${res.url}"></a>`
     })
   }
 })
-
-/*
-fetch('https://api.waifu.pics/sfw/waifu')
-     .then(response => response.json())
-     .then(response => {
-       getContainer.addEventListener('click', function(el){
-         if(el.target.dataset.type === 'generate'){
-           
-         }
-         getContainer.innerHTML = `<div class ="card">
-                       <a href="${response.url}" target="_blank" download="waifu">
-                           <img class="waifu-pic" src=${response.url}>
-                       </a>
-                       <div class="sub-menu">
-                           <h3>Waifu Gacha 2.0</h3>
-                           <button class="generate">Generate</button>
-                       </div>
-                       </div>`
-       
-     })
- })*/
-
-
