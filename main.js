4ce3b8b2773d9f43b1754ec9d7bcce76cb00bf71
Devan <Justdevan1st@gmail.com>
2021-08-05 21:23:48 +0700

    Update Versi 2.0

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-const getImageCard = document.querySelector('.card');
+const getContainer = document.querySelector('.container');
 const getGenerateButton = document.querySelector('.generate');
+const getMainImage = document.querySelector('.main-image')
 
-getGenerateButton.addEventListener('click', function() {
-    getImageCard.innerHTML = ''
-    fetch('https://api.waifu.pics/sfw/waifu')
-        .then(response => response.json())
-        .then(response => {
-            getImageCard.innerHTML = `<a href="${response.url}" download="waifu">
-                <img class="waifu-pic" src=${response.url}>
-            </a>`
-        })
+const requestUrl = (url, success) => {
+  fetch(url).then(res => res.json()).then(res => success(res))
+}
+
+getContainer.addEventListener('click', el => {
+  if(el.target.dataset.type === 'generate'){
+    requestUrl('https://api.waifu.pics/sfw/waifu', res => {
+      getMainImage.innerHTML = ''
+      getMainImage.innerHTML += `<a href="${res.url}" target="_blank"><img class="waifu-pic" src="${res.url}"></a>`
+    })
+  }
 })
+
+/*
+fetch('https://api.waifu.pics/sfw/waifu')
+     .then(response => response.json())
+     .then(response => {
+       getContainer.addEventListener('click', function(el){
+         if(el.target.dataset.type === 'generate'){
+           
+         }
+         getContainer.innerHTML = `<div class ="card">
+                       <a href="${response.url}" target="_blank" download="waifu">
+                           <img class="waifu-pic" src=${response.url}>
+                       </a>
+                       <div class="sub-menu">
+                           <h1>Waifu Gacha 2.0</h1>
+                           <button class="generate">Generate</button>
+                       </div>
+                       </div>`
+       
+     })
+ })*/
+
+
